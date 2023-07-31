94feb5b574b82a57234781ee7de42ffda78ed9e1
KSilverman <kyle@kylesilverman.com>
2023-07-31 12:25:18 -0600

    Adding game state enums

---
+++ src/constants/game.jsx
@@ -0,0 +1,8 @@
+export const STATE = {
+    PRE_START: "Player",
+    PRE_DEAL: "Pre-Deal",
+    DEALER_TURN: "Dealer Turn",
+    PLAYER_TURN: "Player Turn",
+    END_DEAL: "End-Deal",
+    END_SHOE: "End-Shoe"
+}
